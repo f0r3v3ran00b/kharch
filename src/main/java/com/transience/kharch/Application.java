61e7ecf150cbecc2e45eb8af4027c54b4325a354
Sanjay Srinivas <srinivassanjay@gmail.com>
2016-08-07 16:30:53 +1000

    Some refactoring to move Application to different package

--- src/main/java/com/transience/kharch/Application.java
+++ src/main/java/com/transience/kharch/Application.java
@@ -1,24 +1,25 @@
-package hello;
+package com.transience.kharch;
 
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
     }
 
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 }
