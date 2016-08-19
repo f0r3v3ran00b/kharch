c6dbd3971117cff581b0be775c1c4bdfdc3a5d42
Sanjay Srinivas <srinivassanjay@gmail.com>
2016-08-19 23:02:19 +1000

    Stuff

--- src/main/java/com/transience/kharch/Application.java
+++ src/main/java/com/transience/kharch/Application.java
@@ -16,7 +16,7 @@ public class Application {
     public static void main(String[] args) {
 
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        // 
+        //
     }
 
     @RequestMapping("/")
